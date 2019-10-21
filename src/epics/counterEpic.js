24a01a585489f950fa2abc17e6a536008eff48de
Vaios Anastasiadis <vaios_a@outlook.com>
2019-10-21 15:34:58 +0300

    Alternate of pipes in redux observables

--- src/epics/counterEpic.js
+++ src/epics/counterEpic.js
@@ -2,13 +2,13 @@ import { pipe } from 'rxjs'
 import { ofType } from 'redux-observable'
 import { mapTo, delay } from 'rxjs/operators'
 
-const counterIncreaseEpic = pipe(
+const counterIncreaseEpic = pipe( // or  = action$ = action$.pipe(
   ofType('INCREASE'),
   delay(1000),
   mapTo({ type: 'COUNTER_INCREASED' })
 )
 
-const counterDecreaseEpic = pipe(
+const counterDecreaseEpic = pipe( // or  = action$ = action$.pipe(
   ofType('DECREASE'),
   delay(1000),
   mapTo({ type: 'COUNTER_DECREASED' })
